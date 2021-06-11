db78a7a74c2ea5de3cbe2b3e60ebcf5038a1977c
Wilfer Ciro <wilcirom@gmail.com>
2021-06-10 22:15:44 -0500

    first commit

---
+++ components/ConstantServer.js
@@ -0,0 +1,8 @@
+const ConstantServer = {
+	URL_public        : '/api/public/',
+	URL_private       : '/api/private/',
+	URL_server        : 'http://kiwipyme.pythonanywhere.com'
+	//URL_server        : 'http://127.0.0.1:8000'
+};
+
+export default ConstantServer;
