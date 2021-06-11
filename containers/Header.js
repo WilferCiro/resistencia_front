d53ca117bbd31be622b947d171e5354652af6331
Wilfer Ciro <wilcirom@gmail.com>
2021-06-11 09:18:15 -0500

    first commit

--- containers/Header.js
+++ containers/Header.js
@@ -67,6 +67,7 @@ class Header extends BasePanel{
 									return <a key={Math.random()} className={(this.state.selected === item["id"]) ? "selected" : ""} onClick={(e) => this.goTo(item["route"], item["route_alias"], item["id"])}>{item["label"]}</a>
 								})
 							}
+							<a key={Math.random()}>Quimbaya, Quindío</a>
 						</div>
 					</div>
 				</div>
